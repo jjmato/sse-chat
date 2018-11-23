f640983a5566e6080e36afd6e6c91a283561406f
jjmato <jjmatotorres@gmail.com>
2018-11-23 19:45:59 +0100

    clean server code

--- app.js
+++ app.js
@@ -6,71 +6,69 @@ var EventedArray = require('array-events');
 
 var PORT = 3000;
 
-// to support JSON-encoded bodies
-app.use( bodyParser.json() );
-// to support URL-encoded bodies
-app.use(bodyParser.urlencoded({
-	extended: true
-}));
+app.use(bodyParser.json());
+app.use(
+  bodyParser.urlencoded({
+    extended: true
+  })
+);
 
-//config staticos
 app.use('/', express.static('public'));
-
-//TODO:
-//	middleware log
-//	middleware SSE
-
 
 var clientId = 0;
 var clients = {};
 var chat = [
-	{'autor': 'chat',
-	'text': 'hola, bienvenido al chat undefined',
-	'date': new Date()}
+  {
+    autor: 'chat',
+    text: 'hola, bienvenido al chat undefined',
+    date: new Date()
+  }
 ];
 
-function emit(msg){
-
-	console.log("Clients: " + Object.keys(clients) + " <- " + msg);
-	for (clientId in clients) {
-		clients[clientId].write("data: "+ JSON.stringify(msg) + "\n\n"); 
-	};
-
+function emit(msg) {
+  console.log('Clients: ' + Object.keys(clients) + ' <- ' + msg);
+  for (clientId in clients) {
+    clients[clientId].write('data: ' + JSON.stringify(msg) + '\n\n');
+  }
 }
 
-app.get('/api/chat/stream', function (req, res) {
-	console.log('open connection!');
-	req.socket.setTimeout(Number.MAX_VALUE);
-	
-	res.writeHead(200, {
-		'Content-Type': 'text/event-stream',
-		'Cache-Control': 'no-cache'
-	});
-	res.write('\n');
+app.get('/api/chat/stream', function(req, res) {
+  console.log('open connection!');
+  req.socket.setTimeout(Number.MAX_VALUE);
 
-    (function(clientId) {
-        clients[clientId] = res;
-        req.on("close", function(){delete clients[clientId]});
-    })(++clientId)
+  res.writeHead(200, {
+    'Content-Type': 'text/event-stream',
+    'Cache-Control': 'no-cache'
+  });
+  res.write('\n');
 
-	req.on('close', function(){
-		console.log('connection close!')
-	});
+  (function(clientId) {
+    clients[clientId] = res;
+    req.on('close', function() {
+      delete clients[clientId];
+    });
+  })(++clientId);
+
+  req.on('close', function() {
+    console.log('connection close!');
+  });
 });
 
-
-app.get('/api/chat/', function(req, res){
-	res.send(JSON.stringify(chat));
+app.get('/api/chat/', function(req, res) {
+  res.send(JSON.stringify(chat));
 });
 
-app.post('/api/chat', function(req, res){
-	console.log('entra POST...');
-	var msg = {'autor': req.body.aut,'text': req.body.txt,'date': req.body.date || new Date()};
-	chat.push(msg);
-	emit(msg);
-	res.send('200');
-	console.log('... sale POST');
+app.post('/api/chat', function(req, res) {
+  console.log('entra POST...');
+  var msg = {
+    autor: req.body.aut,
+    text: req.body.txt,
+    date: req.body.date || new Date()
+  };
+  chat.push(msg);
+  emit(msg);
+  res.send('200');
+  console.log('... sale POST');
 });
-
 
 app.listen(process.env.PORT || PORT);
